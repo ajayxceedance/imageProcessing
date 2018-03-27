82f7f4fff0f66cbfbc36c9e0bbc75bfe127d8888
Ajay Pal Singh <ajay.singh@xceedance.com>
2018-03-27 11:23:32 +0530

    add server file

---
+++ run_waitress_server.py
@@ -0,0 +1,7 @@
+import os
+from waitress import serve
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imageProcessing.settings")
+application = get_wsgi_application()
+serve(application,host="0.0.0.0",port=os.environ["PORT"])
+
